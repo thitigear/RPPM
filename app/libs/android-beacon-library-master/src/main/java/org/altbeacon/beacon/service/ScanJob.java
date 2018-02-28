d755f5b01ea75aeb4b46eccc7170fdf216f2ad2e
Thiti Jantamongkollert <gera.tht@gmail.com>
2018-02-28 23:20:22 +0700

    Initial commit

---
+++ app/libs/android-beacon-library-master/src/main/java/org/altbeacon/beacon/service/ScanJob.java
@@ -0,0 +1,223 @@
+package org.altbeacon.beacon.service;
+
+import android.annotation.TargetApi;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.bluetooth.le.ScanRecord;
+import android.bluetooth.le.ScanResult;
+import android.os.Build;
+import android.os.Handler;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BuildConfig;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.distance.ModelSpecificDistanceCalculator;
+import org.altbeacon.beacon.logging.LogManager;
+import org.altbeacon.beacon.utils.ProcessUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Used to perform scans periodically using the JobScheduler
+ *
+ * Only one instance of this will be active, even with multiple jobIds.  If one job
+ * is already running when another is scheduled to start, onStartJob gets called again on the same
+ * instance.
+ *
+ * If the OS decides to create a new instance, it will call onStopJob() on the old instance
+ *
+ * Created by dyoung on 3/24/17.
+ * @hide
+ */
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+public class ScanJob extends JobService {
+    private static final String TAG = ScanJob.class.getSimpleName();
+    public static final int PERIODIC_SCAN_JOB_ID = 1;
+    /*
+        Periodic scan jobs are used in general, but they cannot be started immediately.  So we have
+        a second immediate scan job to kick off when scanning gets started or settings changed.
+        Once the periodic one gets run, the immediate is cancelled.
+     */
+    public static final int IMMEDIATE_SCAN_JOB_ID = 2;
+
+    private ScanState mScanState;
+    private Handler mStopHandler = new Handler();
+    private ScanHelper mScanHelper;
+    private boolean mInitialized = false;
+
+    @Override
+    public boolean onStartJob(final JobParameters jobParameters) {
+        initialzeScanHelper();
+        if (jobParameters.getJobId() == IMMEDIATE_SCAN_JOB_ID) {
+            LogManager.i(TAG, "Running immediate scan job: instance is "+this);
+        }
+        else {
+            LogManager.i(TAG, "Running periodic scan job: instance is "+this);
+        }
+
+        List<ScanResult> queuedScanResults = ScanJobScheduler.getInstance().dumpBackgroundScanResultQueue();
+        LogManager.d(TAG, "Processing %d queued scan resuilts", queuedScanResults.size());
+        for (ScanResult result : queuedScanResults) {
+            ScanRecord scanRecord = result.getScanRecord();
+            if (scanRecord != null) {
+                mScanHelper.processScanResult(result.getDevice(), result.getRssi(), scanRecord.getBytes());
+            }
+        }
+        LogManager.d(TAG, "Done processing queued scan resuilts");
+
+        boolean startedScan;
+        if (mInitialized) {
+            LogManager.d(TAG, "Scanning already started.  Resetting for current parameters");
+            startedScan = restartScanning();
+        }
+        else {
+            startedScan = startScanning();
+        }
+        mStopHandler.removeCallbacksAndMessages(null);
+
+        if (startedScan) {
+            LogManager.i(TAG, "Scan job running for "+mScanState.getScanJobRuntimeMillis()+" millis");
+            mStopHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    LogManager.i(TAG, "Scan job runtime expired: " + ScanJob.this);
+                    stopScanning();
+                    mScanState.save();
+                    ScanJob.this.jobFinished(jobParameters , false);
+
+                    // need to execute this after the current block or Android stops this job prematurely
+                    mStopHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            scheduleNextScan();
+                        }
+                    });
+
+                }
+            }, mScanState.getScanJobRuntimeMillis());
+        }
+        else {
+            LogManager.i(TAG, "Scanning not started so Scan job is complete.");
+            ScanJob.this.jobFinished(jobParameters , false);
+        }
+        return true;
+    }
+
+    private void scheduleNextScan(){
+        if(!mScanState.getBackgroundMode()){
+            // immediately reschedule scan if running in foreground
+            LogManager.d(TAG, "In foreground mode, schedule next scan");
+            ScanJobScheduler.getInstance().forceScheduleNextScan(ScanJob.this);
+        } else {
+            startPassiveScanIfNeeded();
+        }
+    }
+
+    private void startPassiveScanIfNeeded() {
+        LogManager.d(TAG, "Checking to see if we need to start a passive scan");
+        boolean insideAnyRegion = false;
+        // Clone the collection before iterating to prevent ConcurrentModificationException per #577
+        List<Region> regions = new ArrayList<>(mScanState.getMonitoringStatus().regions());
+        for (Region region : regions) {
+            RegionMonitoringState state = mScanState.getMonitoringStatus().stateOf(region);
+            if (state != null && state.getInside()) {
+                insideAnyRegion = true;
+            }
+        }
+        if (insideAnyRegion) {
+            // TODO: Set up a scan filter for not detecting a beacon pattern
+            LogManager.i(TAG, "We are inside a beacon region.  We will not scan between cycles.");
+        }
+        else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                mScanHelper.startAndroidOBackgroundScan(mScanState.getBeaconParsers());
+            }
+            else {
+                LogManager.d(TAG, "This is not Android O.  No scanning between cycles when using ScanJob");
+            }
+        }
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        if (params.getJobId() == PERIODIC_SCAN_JOB_ID) {
+            LogManager.i(TAG, "onStopJob called for periodic scan " + this);
+        }
+        else {
+            LogManager.i(TAG, "onStopJob called for immediate scan " + this);
+        }
+        // Cancel the stop timer.  The OS is stopping prematurely
+        mStopHandler.removeCallbacksAndMessages(null);
+        stopScanning();
+        startPassiveScanIfNeeded();
+
+        return false;
+    }
+
+    private void stopScanning() {
+        mInitialized = false;
+        mScanHelper.getCycledScanner().stop();
+        mScanHelper.getCycledScanner().destroy();
+        LogManager.d(TAG, "Scanning stopped");
+    }
+
+    private void initialzeScanHelper() {
+        mScanHelper = new ScanHelper(this);
+        mScanState = ScanState.restore(ScanJob.this);
+        mScanState.setLastScanStartTimeMillis(System.currentTimeMillis());
+        mScanHelper.setMonitoringStatus(mScanState.getMonitoringStatus());
+        mScanHelper.setRangedRegionState(mScanState.getRangedRegionState());
+        mScanHelper.setBeaconParsers(mScanState.getBeaconParsers());
+        mScanHelper.setExtraDataBeaconTracker(mScanState.getExtraBeaconDataTracker());
+        if (mScanHelper.getCycledScanner() == null) {
+            mScanHelper.createCycledLeScanner(mScanState.getBackgroundMode(), null);
+        }
+    }
+
+    // Returns true of scanning actually was started, false if it did not need to be
+    private boolean restartScanning() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mScanHelper.stopAndroidOBackgroundScan();
+        }
+        long scanPeriod = mScanState.getBackgroundMode() ? mScanState.getBackgroundScanPeriod() : mScanState.getForegroundScanPeriod();
+        long betweenScanPeriod = mScanState.getBackgroundMode() ? mScanState.getBackgroundBetweenScanPeriod() : mScanState.getForegroundBetweenScanPeriod();
+        mScanHelper.getCycledScanner().setScanPeriods(scanPeriod,
+                                                      betweenScanPeriod,
+                                                      mScanState.getBackgroundMode());
+        mInitialized = true;
+        if (scanPeriod <= 0) {
+            LogManager.w(TAG, "Starting scan with scan period of zero.  Exiting ScanJob.");
+            mScanHelper.getCycledScanner().stop();
+            return false;
+        }
+
+        if (mScanHelper.getRangedRegionState().size() > 0 || mScanHelper.getMonitoringStatus().regions().size() > 0) {
+            mScanHelper.getCycledScanner().start();
+            return true;
+        }
+        else {
+            mScanHelper.getCycledScanner().stop();
+            return false;
+        }
+    }
+
+    // Returns true of scanning actually was started, false if it did not need to be
+    private boolean startScanning() {
+        BeaconManager beaconManager = BeaconManager.getInstanceForApplication(getApplicationContext());
+        beaconManager.setScannerInSameProcess(true);
+        if (beaconManager.isMainProcess()) {
+            LogManager.i(TAG, "scanJob version %s is starting up on the main process", BuildConfig.VERSION_NAME);
+        }
+        else {
+            LogManager.i(TAG, "beaconScanJob library version %s is starting up on a separate process", BuildConfig.VERSION_NAME);
+            ProcessUtils processUtils = new ProcessUtils(ScanJob.this);
+            LogManager.i(TAG, "beaconScanJob PID is "+processUtils.getPid()+" with process name "+processUtils.getProcessName());
+        }
+        ModelSpecificDistanceCalculator defaultDistanceCalculator =  new ModelSpecificDistanceCalculator(ScanJob.this, BeaconManager.getDistanceModelUpdateUrl());
+        Beacon.setDistanceCalculator(defaultDistanceCalculator);
+        return restartScanning();
+    }
+}
---
+++ app/src/main/java/com/example/gear/rppm/activity/MainActivity.java
@@ -0,0 +1,413 @@
+package com.example.gear.rppm.activity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+
+import com.example.gear.rppm.R;
+import com.example.gear.rppm.fragment.HomeFragment;
+import com.example.gear.rppm.fragment.CheckBeaconFragment;
+import com.example.gear.rppm.fragment.ArmHomeFragment;
+import com.example.gear.rppm.fragment.LegHomeFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    //private FloatingActionButton fab;
+    private Button fragment_home_choice_arms;
+    private Button fragment_home_choice_legs;
+
+    // index to identify current nav menu item
+    public static int navItemIndex = 0;
+
+    // tags used to attach the fragments
+    private static final String TAG_HOME = "home";
+    private static final String TAG_CHECK_BEACON = "check beacon";
+    private static final String TAG_ARM = "arm";
+    private static final String TAG_LEG = "leg";
+    private static final String TAG_SETTINGS = "settings";
+    public static String CURRENT_TAG = TAG_HOME;
+
+    // toolbar titles respected to selected nav menu item
+    private String[] activityTitles;
+
+    // flag to load home fragment when user presses back key
+    private boolean shouldLoadHomeFragOnBackPress = true;
+    private Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //add toolbar to app
+        toolbar = (Toolbar) findViewById(R.id.toolbar); //in app_bar_sliding_menu_xml
+        setSupportActionBar(toolbar);
+
+        //selectToolbarNav();
+
+        mHandler = new Handler();
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        //fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        // load toolbar titles from string resources
+        activityTitles = getResources().getStringArray(R.array.nav_item_activity_titles);
+
+        /**fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadNewRoundFragmentByFab();
+            }
+        });*/
+
+
+        fragment_home_choice_arms = (Button) findViewById(R.id.fragment_home_content_choice_arms);
+        fragment_home_choice_legs = (Button) findViewById(R.id.fragment_home_content_choice_legs);
+
+
+        /**fragment_home_choice_arms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadArmFragment();
+            }
+        });*/
+
+
+        /**fragment_home_choice_legs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadLegFragment();
+            }
+        });*/
+
+
+        setUpNavigationView();
+
+        if (savedInstanceState == null) {
+            navItemIndex = 0;
+            CURRENT_TAG = TAG_HOME;
+            loadFragment();
+        }
+
+        //TextView out_armL = findViewById(R.id.home_fragment_output_armL);
+
+
+
+
+    }
+
+    /***
+     * Load navigation menu header information
+     * like background image, profile image
+     * name, website, notifications action view (dot)
+     */
+
+    private void loadFragment() {
+        // selecting appropriate nav menu item
+        selectNavMenu();
+
+        // set toolbar title
+        setToolbarTitle();
+
+        // if user select the current navigation menu again, don't do anything
+        // just close the navigation drawer
+        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
+            drawer.closeDrawers();
+
+            // show or hide the fab button
+            /**toggleFab();*/
+            return;
+        }
+
+        // Sometimes, when fragment has huge data, screen seems hanging
+        // when switching between navigation menus
+        // So using runnable, the fragment is loaded with cross fade effect
+        // This effect can be seen in GMail app
+        Runnable mPendingRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // update the main content by replacing fragments
+                Fragment fragment = getFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
+                fragmentTransaction.commitAllowingStateLoss();
+            }
+        };
+
+        // If mPendingRunnable is not null, then add to the message queue
+        if (mPendingRunnable != null) {
+            mHandler.post(mPendingRunnable);
+        }
+
+        // show or hide the fab button
+        /**toggleFab();*/
+
+        //Closing drawer on item click
+        drawer.closeDrawers();
+
+        // refresh toolbar menu
+        invalidateOptionsMenu();
+    }
+
+    private Fragment getFragment() {
+        switch (navItemIndex) {
+            case 0:
+                // home fragment
+                HomeFragment homeFragment = new HomeFragment();
+                return homeFragment;
+            case 1:
+                // check beacon fragment
+                CheckBeaconFragment checkBeaconFragment = new CheckBeaconFragment();
+                return checkBeaconFragment;
+            case 2:
+                // arm fragment
+                ArmHomeFragment armHomeFragment = new ArmHomeFragment();
+                return armHomeFragment;
+            case 3:
+                // leg fragment
+                LegHomeFragment legHomeFragment = new LegHomeFragment();
+                return legHomeFragment;
+
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    private void setToolbarTitle() {
+        getSupportActionBar().setTitle(activityTitles[navItemIndex]);
+    }
+
+    private void selectNavMenu() {
+        //navigationView.getMenu().getItem(navItemIndex).setChecked(true);
+        //navigationView.getMenu().getItem(navItemIndex);
+        navigationView.getMenu().getItem(navItemIndex);
+    }
+
+    private void setUpNavigationView() {
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.nav_home:
+                        navItemIndex = 0;
+                        CURRENT_TAG = TAG_HOME;
+                        break;
+                    case R.id.nav_check_beacon:
+                        navItemIndex = 1;
+                        CURRENT_TAG = TAG_CHECK_BEACON;
+                        break;
+                    case R.id.nav_arm:
+                        navItemIndex = 2;
+                        CURRENT_TAG = TAG_ARM;
+                        break;
+                    case R.id.nav_leg:
+                        navItemIndex = 3;
+                        CURRENT_TAG = TAG_LEG;
+                        break;
+                    default:
+                        navItemIndex = 0;
+                }
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if (menuItem.isChecked()) {
+                    menuItem.setChecked(false);
+                } else {
+                    menuItem.setChecked(true);
+                }
+                menuItem.setChecked(true);
+
+                loadFragment(); // old loadHomeFragment
+
+                return true;
+            }
+        });
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawer.setDrawerListener(actionBarDrawerToggle);
+
+
+        //calling sync state is necessary or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawers();
+            return;
+        }
+
+        // This code loads home fragment when back key is pressed
+        // when user is in other fragment than home
+        if (shouldLoadHomeFragOnBackPress) {
+            // checking if user is on other navigation menu
+            // rather than home
+            if (navItemIndex != 0) {
+                navItemIndex = 0;
+                CURRENT_TAG = TAG_HOME;
+                loadFragment();
+                return;
+            }
+        }
+        super.onBackPressed();
+    }
+
+    // show or hide the fab
+    /**private void toggleFab() {
+        if (navItemIndex == 0)
+            fab.show();
+        else
+            fab.hide();
+    }*/
+
+    private void loadArmFragment(){
+        navItemIndex = 2;
+        CURRENT_TAG = TAG_ARM;
+        selectNavMenu();
+        setToolbarTitle();
+        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
+            drawer.closeDrawers();
+
+            // show or hide the fab button
+            /**toggleFab();*/
+        }
+
+        Runnable mPendingRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // update the main content by replacing fragments
+                Fragment fragment = getFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
+                fragmentTransaction.commitAllowingStateLoss();
+            }
+        };
+
+        // If mPendingRunnable is not null, then add to the message queue
+        if (mPendingRunnable != null) {
+            mHandler.post(mPendingRunnable);
+        }
+        /**toggleFab();*/
+        drawer.closeDrawers();
+        invalidateOptionsMenu();
+    }
+
+    private void loadLegFragment(){
+        navItemIndex = 3;
+        CURRENT_TAG = TAG_LEG;
+        selectNavMenu();
+        setToolbarTitle();
+        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
+            drawer.closeDrawers();
+
+            // show or hide the fab button
+            /**toggleFab();*/
+        }
+
+        Runnable mPendingRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // update the main content by replacing fragments
+                Fragment fragment = getFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
+                fragmentTransaction.commitAllowingStateLoss();
+            }
+        };
+
+        // If mPendingRunnable is not null, then add to the message queue
+        if (mPendingRunnable != null) {
+            mHandler.post(mPendingRunnable);
+        }
+        /**toggleFab();*/
+        drawer.closeDrawers();
+        invalidateOptionsMenu();
+    }
+
+    /**    private void scanBeacon(){
+
+     final TextView shin = findViewById(R.id.home_fragment_output_shinL);
+
+     bluetoothLeScanner.startScan(new ScanCallback() {
+    @Override
+    public void onScanResult(int callbackType, ScanResult result) {
+    super.onScanResult(callbackType, result);
+
+    shin.setText("" + deviceList.size());
+    scanResult = result;
+
+    if (scanResult.getDevice().getName() != null) {
+    int[] capsuleRssiTxPower = {scanResult.getRssi(), scanResult.getScanRecord().getTxPowerLevel()};
+
+    Log.e(TAG, "******************* Device Key : " +
+    deviceList.keySet());
+    if (deviceList.containsKey(scanResult.getDevice().getAddress())) {
+    /* Update Device Rssi & TxPowerLevel */
+/**                        deviceList.put(scanResult.getDevice().getAddress(), capsuleRssiTxPower);
+
+ } else {
+ /* Add Device to DeviceList */
+/**                        deviceList.put(scanResult.getDevice().getAddress(), capsuleRssiTxPower);
+ }
+
+ setCoreF_deviceList();
+
+ }
+ }
+ });
+ }*/
+
+}
